83dc2c99294724f174363b133d45d1f36236fd59
Daniel Vasylenko <danielspr3@gmail.com>
2020-04-05 21:39:42 +0200

    Returned back the incognito button

--- src/views/Application.js
+++ src/views/Application.js
@@ -1,5 +1,8 @@
-import React from 'react';
+import React, { useMemo } from 'react';
 import styled from '@emotion/styled';
+import IncognitoIcon from 'mdi-react/IncognitoIcon';
+import IncognitoOffIcon from 'mdi-react/IncognitoOffIcon';
+import CrossHairsGpsIcon from 'mdi-react/CrosshairsGpsIcon';
 import { withAuth } from '../hocs/withAuth';
 import { Button } from '../components/Button';
 import { useSocket } from '../contexts/SocketContext';
@@ -11,22 +14,58 @@ import { Navbar } from '../components/Navbar';
 
 const MapWrapper = styled.div`
   width: 100%;
-  height: 100vh;
+  height: calc(100vh - 60px);
   position: relative;
 `;
 
+const RoundFloatingButton = ({ icon, ...props }) => {
+  const Icon = styled(icon)`
+    background: white;
+    padding: 1rem;
+    border-radius: 50%;
+    box-shadow: 0px 0px 10px lightgrey;
+  `;
+
+  return <Icon {...props} />;
+};
+
+const FloatingButtonsList = styled.ul`
+  list-style-type: none;
+  margin: 0;
+  padding: 0;
+  position: absolute;
+  bottom: 50px;
+  right: .7rem;
+  display: flex;
+  flex-direction: column;
+
+  svg:first-child {
+    margin-bottom: 1rem;
+  }
+`;
+
 const Map = () => {
-  return (
+  const settings = useSettings();
+  return useMemo(() => (
     <MapWrapper>
       <Mapbox/>
+      <FloatingButtonsList>
+        <RoundFloatingButton
+          icon={CrossHairsGpsIcon}
+          onClick={() => {}}
+        />
+        <RoundFloatingButton
+          icon={settings.isPublicProfile ? IncognitoIcon : IncognitoOffIcon}
+          onClick={settings.toggleProfileVisibility}
+        />
+      </FloatingButtonsList>
     </MapWrapper>
-  );
+  ), [settings.isPublicProfile]);
 };
 
 const Application = ({ children }) => {
   const auth = useAuth();
   const socketApi = useSocket();
-  const settings = useSettings();
 
   const onLogout = (event) => {
     auth.logout();
